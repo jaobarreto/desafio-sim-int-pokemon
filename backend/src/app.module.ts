2ffed984cce8656649110b1e8fc4dfd1171b8555
jaobarreto <joaobarretoprof@gmail.com>
2026-03-08 16:32:31 -0300

    chore: configurar typeorm e módulos principais do nestjs

---
+++ backend/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module.js';
+import { PokemonsModule } from './pokemons/pokemons.module.js';
+import { AuthModule } from './auth/auth.module.js';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // disable in production
+      }),
+    }),
+
+    UsersModule,
+    PokemonsModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
